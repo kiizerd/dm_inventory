29e3c525ea99764966f0055cf4a470a8b1bb2d22
Kiizerd <ajsewell13@gmail.com>
2025-10-24 00:04:56 -0500

    Setup MantineUI

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,18 @@
-import Inventory from './components/Inventory/Inventory.tsx';
 import './App.css';
+import '@mantine/core/styles.css';
+import { createTheme, MantineProvider } from '@mantine/core';
+import Inventory from './components/Inventory/Inventory.tsx';
 
 const apiBase = (import.meta.env.VITE_API_BASE as string) || '/api';
 
+const theme = createTheme({});
+
 function App() {
-  return <Inventory endpoint={apiBase + '/inventory'} />;
+  return (
+    <MantineProvider theme={theme} defaultColorScheme="dark">
+      <Inventory endpoint={apiBase + '/inventory'} />
+    </MantineProvider>
+  );
 }
 
 export default App;
