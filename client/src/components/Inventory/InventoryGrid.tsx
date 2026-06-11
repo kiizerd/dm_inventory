7d0c9bff2c9d9b97dfff5ba3b937dcaeb461df83
Kiizerd <ajsewell13@gmail.com>
2026-06-11 13:52:56 -0500

    Adjust inventory grid layout

--- client/src/components/Inventory/InventoryGrid.tsx
+++ client/src/components/Inventory/InventoryGrid.tsx
@@ -6,19 +6,18 @@ type Props = {
 };
 
 export default function InventoryGrid({ items }: Props) {
+  // const page = items.map;
   return (
-    <div>
-      <div className="container mx-auto pt-6">
-        {items.length === 0 ? (
-          <div className="text-gray-400">No matching items.</div>
-        ) : (
-          <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
-            {items.map((v) => (
-              <InventoryCard key={v.stk} vehicle={v} />
-            ))}
-          </div>
-        )}
-      </div>
+    <div className="container mx-auto pt-6">
+      {items.length === 0 ? (
+        <div className="text-gray-400">No matching items.</div>
+      ) : (
+        <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
+          {items.map((v) => (
+            <InventoryCard key={v.stk} vehicle={v} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
